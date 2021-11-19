391ae6a539c998af80f09d5b17c08f4100706318
morais948 <progmatheusmorais@gmail.com>
2021-11-18 22:42:18 -0300

    first :rocket:

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> VueJS </v-list-item-title>
+          <v-list-item-subtitle> Menu </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link :to="item.to">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+
+
+    <v-app-bar
+        app
+        color="teal lighten-3"
+        dark
+        prominent
+        src="./assets/images/bg.png"
+    >
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>Title</v-toolbar-title>
+    </v-app-bar>
+    
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Tarefas", icon: "mdi-view-dashboard", to: "/" },
+      { title: "Sobre", icon: "mdi-help-box", to: "/Sobre" },
+    ],
+    right: null,
+  }),
+};
+</script>
